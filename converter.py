184f7b9a367ac58651f22a6d654e0fa4d4c7a4fa
Carlos garcia <carlosfelgarcia@gmail.com>
2024-06-27 17:28:35 -0700

    Update requirements.txt to include openpyxl dependency Refactor setup.py to add a setup_alias function for creating an alias in .zshrc

--- converter.py
+++ converter.py
@@ -1,7 +1,6 @@
 # MIT License (c) 2024. Carlos Garcia, www.carlosgarciadev.com
 import json
 import pathlib
-import tkinter as tk
 from hashlib import md5
 
 import ofxparse
@@ -9,13 +8,9 @@ import pandas as pd
 
 
 class Converter:
-    def __init__(self, files_path: pathlib.Path | None, info_label: tk.Label | None = None) -> None:
+    def __init__(self, files_path: pathlib.Path) -> None:
         self.ofx_parser = ofxparse.OfxParser()
-        self._info_label = info_label
-        if not files_path:
-            self.files_path = pathlib.Path.home() / "Downloads"
-        else:
-            self.files_path = files_path
+        self.files_path = files_path
 
     def consistent_hash(self, string):
         result = abs(int(md5(string.encode()).hexdigest(), 16))
@@ -26,10 +21,7 @@ class Converter:
         excel_files = list(self.files_path.glob("*.xlsx"))
 
         if not excel_files:
-            if self._info_label:
-                self._info_label["text"] = "No Excel files found."
-            else:
-                print("No Excel files found.")
+            print("No Excel files found.")
             return
 
         if not excel_trans_path.exists():
@@ -38,10 +30,7 @@ class Converter:
         all_transactions = []
 
         for excel_file in excel_files:
-            if self._info_label:
-                self._info_label["text"] = f"Processing {excel_file}..."
-            else:
-                print(f"Processing {excel_file}...")
+            print(f"Processing {excel_file}...")
             df = pd.read_excel(excel_file)
 
             df["Settlement Date"] = pd.to_datetime(df["Settlement Date"], format="%Y-%m-%d %I:%M:%S %p").dt.strftime(
@@ -85,19 +74,13 @@ class Converter:
         with open(json_file_path, "w") as file:
             json.dump(all_transactions, file, indent=4)
 
-        if self._info_label:
-            self._info_label["text"] = "Excel files converted successfully."
-        else:
-            print("Excel files converted successfully.")
+        print("Excel files converted successfully.")
 
     def create_transaction_files_qfx(self) -> None:
         all_transactions = []
         qfx_transaction_files = list(self.files_path.rglob("*.QFX")) + list(self.files_path.rglob("*.qfx"))
         if not qfx_transaction_files:
-            if self._info_label:
-                self._info_label["text"] = "No QFX files found."
-            else:
-                print("No QFX files found.")
+            print("No QFX files found.")
             return
 
         qfx_trans_path = self.files_path / "qfx_transactions"
@@ -105,10 +88,7 @@ class Converter:
             qfx_trans_path.mkdir()
 
         for qfx_file in qfx_transaction_files:
-            if self._info_label:
-                self._info_label["text"] = f"Processing {qfx_file}..."
-            else:
-                print(f"Processing {qfx_file}...")
+            print(f"Processing {qfx_file}...")
             with open(qfx_file) as file:
                 ofx_data = self.ofx_parser.parse(file)
 
@@ -119,10 +99,7 @@ class Converter:
                 accounts = [ofx_data.account]
 
             for account in accounts:
-                if self._info_label:
-                    self._info_label["text"] = f"Processing account {account.account_id}..."
-                else:
-                    print(f"Processing account {account.account_id}...")
+                print(f"Processing account {account.account_id}...")
                 transactions = []
                 for transaction in account.statement.transactions:
                     name = transaction.payee.strip()
@@ -146,7 +123,4 @@ class Converter:
         with open(json_file_path, "w") as file:
             json.dump(all_transactions, file, indent=4)
 
-        if self._info_label:
-            self._info_label["text"] = "QFX files converted successfully."
-        else:
-            print("QFX files converted successfully.")
+        print("QFX files converted successfully.")
